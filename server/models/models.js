0ac22d0056278e65fd93ffbb32c3b5e711dc44e8
Михаил Цепляев <mnwa@Noutbuk-Mihail-4.local>
2026-02-18 01:11:07 +0300

    Начало работы с backend и подключение bd

---
+++ server/models/models.js
@@ -0,0 +1,178 @@
+const sequelize=require('../db')
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.TEXT
+    },
+    role:{
+        type: DataTypes.STRING,
+        defaultValue: "USER"
+    },
+}, {
+    timestamps: true
+})
+
+const Film = sequelize.define('films', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT
+    },
+    posterUrl: {
+        type: DataTypes.STRING
+    },
+    releaseDate: {
+        type: DataTypes.DATEONLY
+    },
+    rating: {
+        type: DataTypes.DECIMAL(3, 1),
+        defaultValue: 0
+    },
+    genres: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
+    },
+    director: {
+        type: DataTypes.STRING
+    },
+    actors: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
+    },
+    duration: {
+        type: DataTypes.INTEGER
+    },
+}, {
+    timestamps: true
+})
+
+const Cinema = sequelize.define('cinemas', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    coordinates: {
+        type: DataTypes.GEOGRAPHY('POINT'),
+        allowNull: false
+    },
+    phoneNumber: {
+        type: DataTypes.STRING
+    },
+    openHours: {
+        type: DataTypes.JSONB
+    },
+}, {
+    timestamps: true
+})
+
+const UserFilmRelationship = sequelize.define('user_film_relationships', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    film_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'films',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true
+})
+
+const UserCinemaRelationship = sequelize.define('user_cinema_relationships', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    cinema_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'cinemas',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true
+})
+
+const FilmCinemaRelationship = sequelize.define('film_cinema_relationships', {
+    film_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'films',
+            key: 'id'
+        }
+    },
+    cinema_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'cinemas',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true
+})
+
+User.belongsToMany(Film, { through: UserFilmRelationship });
+Film.belongsToMany(User, { through: UserFilmRelationship });
+
+User.belongsToMany(Cinema, { through: UserCinemaRelationship });
+Cinema.belongsToMany(User, { through: UserCinemaRelationship });
+
+Film.belongsToMany(Cinema, { through: FilmCinemaRelationship });
+Cinema.belongsToMany(Film, { through: FilmCinemaRelationship });
+
+module.exports = {
+    User, 
+    Film, 
+    Cinema,
+    UserFilmRelationship,
+    UserCinemaRelationship,
+    FilmCinemaRelationship
+}
